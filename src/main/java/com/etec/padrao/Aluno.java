1418efaef301445650c1a603a213f3536e47fa88
JoaoMayta <tohada12.com>
2022-08-28 11:05:18 -0300

    primeiro commit

---
+++ src/main/java/com/etec/padrao/Aluno.java
@@ -0,0 +1,18 @@
+package com.etec.padrao;
+
+public class Aluno extends Pessoa
+{
+    private boolean fumante = false;
+    
+    public Aluno(){
+    
+    }
+    
+    public boolean isFumante(){
+        return fumante;
+    }
+    
+    public void setFumante(boolean fumante){
+        this.fumante = fumante;
+    }
+}
---
+++ src/main/java/com/etec/util/IO.java
@@ -0,0 +1,28 @@
+package com.etec.util;
+
+import javax.swing.JOptionPane;
+
+public class IO {
+    private String texto;
+    public IO()
+    {
+    
+    }
+    public String getTexto()
+    {
+        return texto;
+    }
+    public void setTexto(String texto)
+    {
+        this.texto = texto;
+    }
+    public void exibirTexto()
+    {
+        JOptionPane.showMessageDialog(null, texto);
+    }
+    public String digitarTexto(String mensagem)
+    {
+        this.setTexto(JOptionPane.showInputDialog(mensagem));
+        return this.getTexto();
+    }
+}
